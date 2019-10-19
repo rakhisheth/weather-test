e58d1cd51963d4a3c7729df40f8d44162426ab74
Rakhi Sheth <rakhi.sheth@outlook.com>
2019-10-19 14:16:06 +0100

    Updates

---
+++ precache-manifest.04e2c55c0c4768706e462b51b0119ba6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38635d4e0e5f9477f113bd7eb1c6b0ef",
+    "url": "/weather-test/index.html"
+  },
+  {
+    "revision": "1deeb67fdce97159a383",
+    "url": "/weather-test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1ca99d773090cf6e7493",
+    "url": "/weather-test/static/js/2.82afb0bd.chunk.js"
+  },
+  {
+    "revision": "1deeb67fdce97159a383",
+    "url": "/weather-test/static/js/main.5e01a36d.chunk.js"
+  },
+  {
+    "revision": "b52c8486ebd42de5af2c",
+    "url": "/weather-test/static/js/runtime-main.83c516c3.js"
+  }
+]);
